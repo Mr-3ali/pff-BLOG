84794d8a130f20de4e69097a1524e30438047c66
Mr-3ali <marocaina09@gmailcom>
2024-05-18 16:36:53 +0100

    fixed

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,4 @@ Route::post('/categories', [CategoryController::class, 'store'])->name('categori
 Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
 Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');    
+Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
